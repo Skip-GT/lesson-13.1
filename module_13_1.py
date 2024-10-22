bf7435dccc2cad1c555d28191c58034c3f4e793b
Skip-GT <yaroslav.vasilev.2015@gmail.com>
2024-10-22 19:04:13 +0300

    Add files via upload

---
+++ module_13_1.py
@@ -0,0 +1,20 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f"Силач {name} начал соревнования.")
+    for i in range(1, 6):
+        await asyncio.sleep(1 / power)
+        print(f"Силач {name} поднял {i} шар.")
+    print(f"Силач {name} закончил соревнования.")
+
+
+async def start_tournament():
+    tasks = start_strongman('Pasha', 3),
+    tasks_1 = start_strongman('Denis', 4),
+    tasks_2 = start_strongman('Apollon', 5)
+    await asyncio.gather(*tasks, *tasks_1, tasks_2)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_tournament())
